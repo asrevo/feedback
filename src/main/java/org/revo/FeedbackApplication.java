2dccfd918ffb29f56cdbde6239949ef038bc40f9
ashraf <ashraf1abdelrasool@gmail.com>
2018-12-21 10:49:08 +0200

    push all

--- src/main/java/org/revo/FeedbackApplication.java
+++ src/main/java/org/revo/FeedbackApplication.java
@@ -1,9 +1,11 @@
 package org.revo;
 
+import org.revo.Config.Processor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -12,6 +14,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableMongoAuditing
 @EnableCaching
 @EnableScheduling
+@EnableBinding({Processor.class})
 public class FeedbackApplication {
     public static void main(String[] args) {
         SpringApplication.run(FeedbackApplication.class, args);
